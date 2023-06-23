27036f424f28f1b59fd4651b7075ebb1242bbb45
dangmauquang <dangmauquang149@gmail.com>
2023-06-23 10:45:52 +0700

    Upload final project 23.06.2023

---
+++ Spring_MVC_Project_Final/src/main/java/com/mycompany/spring_mvc_project_final/enums/ProductStatus.java
@@ -0,0 +1,12 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.spring_mvc_project_final.enums;
+
+public enum ProductStatus {
+    ACTIVE,//san pham dang ban'
+    PAUSED,//san pham tam dung ban'
+    DISABLED//san pham da het ban'
+}
---
+++ Spring_MVC_Project_Final/src/main/java/com/mycompany/spring_mvc_project_final/repository/FavoriteRepository.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.spring_mvc_project_final.repository;
+
+import com.mycompany.spring_mvc_project_final.entities.FavoriteEntity;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FavoriteRepository extends JpaRepository<FavoriteEntity, Integer>{
+    
+    public List<FavoriteEntity> findByAccountId(int accountId);
+}
